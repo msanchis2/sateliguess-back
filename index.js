bbb44c2c6915b2f35ab84a75e9d47a72b017f4c9
0021372 <0021372@viewnext.com>
2025-03-17 20:34:02 +0100

    a

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import "dotenv/config";
-import express from 'express';
-import apiRoutes from './src/router/api.routes.js';
-import cors from 'cors';
-import { serve, setup } from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc'
-import db from './src/config/db.config.js'
+import express from "express";
+import apiRoutes from "./src/router/api.routes.js";
+import cors from "cors";
+import { serve, setup } from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import db from "./src/config/db.config.js";
 
 const app = express();
 const initDB = async () => {
@@ -13,9 +13,8 @@ const initDB = async () => {
 
 // Configurar CORS
 const allowedOrigins = [
-  process.env.ORIGIN || 'http://localhost:4000', 
-  'http://85.208.16.214',  
-  'https://85.208.16.214', 
+  process.env.ORIGIN,
+  "http://localhost:4000",
 ];
 app.use(
   cors({
@@ -23,36 +22,36 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        console.log('Solicitud desde:', origin);
-        callback(new Error('Chinga tu puta madre'));
+        console.log("Solicitud desde:", origin);
+        callback(new Error("Chinga tu puta madre"));
       }
     },
-    methods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true, 
+    methods: ["GET", "HEAD", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
 // Documentación Swagger
 const swaggerOptions = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'Documentación de la API',
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "Documentación de la API",
     },
   },
-  apis: ['./src/router/*.js'],
+  apis: ["./src/router/*.js"],
 };
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', serve, setup(swaggerDocs));
+app.use("/api-docs", serve, setup(swaggerDocs));
 
 // Rutas de la API
-app.use('/api', apiRoutes);
-app.options('*', cors());
+app.use("/api", apiRoutes);
+app.options("*", cors());
 
 // Inicialización de la base de datos y servidor
 initDB().then(() => {
-  app.listen( process.env.PORT || 3000, () => console.log('ATENCIÓN: Hola'));
+  app.listen(process.env.PORT || 3000, () => console.log("ATENCIÓN: Hola"));
 });
